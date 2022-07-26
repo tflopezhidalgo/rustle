3cdd9729ecc7e7083c3253a3de940d5687b08881
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-07-26 01:49:47 -0300

    Add random word picking

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::fs;
 use std::collections::HashSet;
 use std::io::{stdin, stdout, Write};
 
-// Our best on picking a random word
-static WORD: &str = "chair";
+use rand::Rng;
+
 
 type Letter = char;
 type Word = String;
@@ -14,6 +15,8 @@ struct GameState {
     guesses: Vec<Word>,
     misses: HashSet<Letter>,
     hits: HashSet<Letter>,
+    target: String,
+    errors: HashSet<UserInputError>,
 }
 
 static GAME_TITLE: &str = r"
@@ -31,14 +34,30 @@ enum TextColor {
     Green,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum UserInputError {
+    InvalidLength
+}
+
 fn should_finish(game: &GameState) -> bool {
-    game.player_won
+    game.player_won || game.guesses.len() >= 5
 }
 
 fn process_player_guess(guess: String, state: &GameState) -> GameState {
     let mut new_state = state.clone();
 
-    if guess == String::from(WORD) {
+    // If player guess is empty just skip it.
+    if guess.len() == 0 {
+        return new_state;
+    }
+
+    // If it does not have the right lenght, skip it.
+    if guess.len() > 5 {
+        new_state.errors.insert(UserInputError::InvalidLength);
+        return new_state;
+    }
+
+    if guess == new_state.target {
         new_state.player_won = true;
         return new_state;
     }
@@ -46,7 +65,7 @@ fn process_player_guess(guess: String, state: &GameState) -> GameState {
     new_state.guesses.push(guess.clone());
 
     guess.chars().for_each(|l| {
-        match String::from(WORD).contains(l) {
+        match new_state.target.contains(l) {
             true => new_state.hits.insert(l),
             false => new_state.misses.insert(l),
         };
@@ -86,14 +105,20 @@ fn ask_for_word() -> std::io::Result<String> {
     let mut word = String::new();
     stdin().read_line(&mut word)?;
 
+    // Sanitize the input.
+
     // Pop the final \n.
     word.pop();
 
-    Ok(word)
+    Ok(String::from(word.trim()))
 }
 
 fn clear_screen() -> () {
     print!("{}[2J{esc}[1;1H", esc = 27 as char);
+}
+
+fn format_missed_word(w: &String) -> String {
+    format!("+ {}  ❌", w)
 }
 
 fn show_ui(game: &GameState) -> () {
@@ -107,7 +132,13 @@ fn show_ui(game: &GameState) -> () {
 
     // Show missed words.
     game.guesses.iter().for_each(|g| {
-        println!("+ {}  ❌", g);
+        println!("{}", format_missed_word(g));
+    });
+
+    game.errors.iter().for_each(|e| {
+        match e {
+            UserInputError::InvalidLength => println!("Invalid length!"),
+        }
     });
 
     println!();
@@ -125,19 +156,41 @@ fn main_loop(game: GameState) -> std::io::Result<()> {
         let guess = ask_for_word()?;
 
         g = process_player_guess(guess, &g);
-
-        if g.player_won {
-            println!("You won! 🤟");
-        }
     }
+
+    if g.player_won {
+        println!("You won! 🤟");
+    } else {
+        println!("You lost! X");
+    }
+
     Ok(())
 }
 
+fn pick_word() -> String {
+    let content = fs::read_to_string("src/words.txt").unwrap();
+
+    let line = rand::thread_rng().gen_range(0..content.lines().count());
+
+    for (i, l) in content.lines().enumerate() {
+        if i == line {
+            return String::from(l);
+        }
+    }
+
+    return String::from("invalid");
+}
+
 fn main() -> std::io::Result<()> {
+
+    let target: String = pick_word();
+
     main_loop(GameState {
+        target,
         guesses: Vec::new(),
         misses: HashSet::new(),
         hits: HashSet::new(),
+        errors: HashSet::new(),
         user_requested_finish: false,
         player_won: false,
     })?;
